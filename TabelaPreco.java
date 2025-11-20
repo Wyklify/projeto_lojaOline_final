3a8859702a0ee1abec87c12efff6fa731a3d478d
Wyklify Jhonson <wyklifyjhonson01@gmail.com>
2025-11-20 10:40:52 -0300

    adicionado melhoria para aplicar melhor promocao ativa para o preco e procurar o preco padrao caso não tenha promoção ativa

--- TabelaPreco.java
+++ TabelaPreco.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TabelaPreco {
-    
+
     private String produtoId;
     private List<Preco> precos;
 
@@ -12,7 +12,7 @@ public class TabelaPreco {
         this.precos = new ArrayList<>();
     }
 
-    public void adiconarPreco(Preco preco) {
+    public void adicionarPreco(Preco preco) {
 
         precos.add(preco);
     }
@@ -21,16 +21,37 @@ public class TabelaPreco {
 
         // verifica se tem promocao ativa hoje
 
+        Preco melhorPromocao = null;
+
+        for (Preco preco : precos) {
+
+            if (preco.temVigencia() && preco.estaVigente(hoje)) {
+
+                if (melhorPromocao == null || preco.getValor() < melhorPromocao.getValor()) {
+
+                    melhorPromocao = preco;
+                }
+            }
+        }
+
+        if (melhorPromocao != null) {
+
+            return melhorPromocao.getValor();
+            
+        }
+
+        // se não houver promoção, procura o preco padrão
+
         for (Preco preco : precos) {
             
-            if (preco.temVigencia() && preco.estaVigente(hoje)) {
+            if (!preco.temVigencia()) {
                 
                 return preco.getValor();
             }
         }
 
-        throw new RuntimeException("Nehum preço encontrado para o produto" + produtoId);
+        // não se não tiver preço
+        throw new RuntimeException("Nehum preço encontrado para o produto " + produtoId);
     }
 
-    
 }
